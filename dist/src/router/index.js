fb604e0c585951b35261a8b94a627f0e8b9348f7
Parichay Mandal <parichay@setup.technology>
2019-01-28 16:06:11 +0600

    Altering index.html

---
+++ dist/src/router/index.js
@@ -0,0 +1,4 @@
+Html Webpack Plugin:
+<pre>
+  Error: The loader "/Users/parichay/parichaymandal.github.io/node_modules/html-  webpack-plugin/lib/loader.js!/Users/parichay/parichaymandal.github.io/index.ht  ml" didn't return html.
+</pre>
